9b9c20ed04c3e19bd019345d0945c37cb70b2256
Theophilus Omorebee <theo4u@ymail.com>
2017-05-03 23:12:13 +0100

    done with integration of clarify with our project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
+
+import { ClarityModule  } from 'clarity-angular';
+
 
 import { AppComponent } from './app.component';
 
@@ -12,8 +17,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
-  ],
+    HttpModule,
+    BrowserAnimationsModule,
+    ClarityModule.forRoot()
+],
   providers: [],
   bootstrap: [AppComponent]
 })
